134e12821926b68d9542d920e785dbbaf21d410c
ACRavenovo <3152304688@qq.com>
2024-10-22 08:31:02 +0800

    first update

---
+++ MyStl/MyStl/MyVector.cpp
@@ -0,0 +1,21 @@
+#ifndef __Vector__
+#define __Vector__
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+static const size_t npos = -1;
+static const float SpaceMutiConstant = 1.5;
+#define iterator char*
+using std::ostream;
+using std::istream;
+
+template<class T, class T1>
+class Vector {
+private:
+
+
+public:
+
+};
+
+
+#endif
